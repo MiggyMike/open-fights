670810fd8b101c048da4f81187a4aeea2d30407d
MICHAEL <MJENNINGS6@gmail.com>
2021-06-09 10:51:58 -0400

    created models and database for tables

---
+++ app/models/airline.rb
@@ -0,0 +1,17 @@
+class Airline < ApplicationRecord
+  has_many :reviews # handles the relationship with Review Model
+
+  before_create :slugify #set slug for airline before created in db
+
+  ## slug airline name
+  # creeate slugify method
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  # method to caputre avg score
+  def avg_score
+    reviews.averae(:score).round(2).to_f
+  end
+
+end
