67c1065458fcba9fa23dc1d96d80fc41bb6b5ca0
MICHAEL <MJENNINGS6@gmail.com>
2021-06-09 16:20:47 -0400

    Basic Routes staged

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  ## set Route Path
+  root 'pages#index'
+  #namespace API
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # routes request that are NOT defined in api back to index path - 
+  get '*path', to: 'pages#index', via: :all
 end
