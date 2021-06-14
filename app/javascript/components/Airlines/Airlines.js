7552d096526390f7a5f86a93826c2764bec2fa74
MICHAEL <MJENNINGS6@gmail.com>
2021-06-13 22:02:03 -0400

    built out Airlines component w/useEffect useState, and axios fetch data

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
 
 const Airlines = () => {
-  return <div> This is the Airlines#index view for the app</div>;
+  const [airlines, setAirlines] = useState([]);
+
+  useEffect(() => {
+    // Get all of the airlines from api
+    // Update airlines in out state
+
+    axios
+      .get('/api/v1/airlines.json')
+      // .then((resp) => console.log(resp))
+      .then((resp) => {
+        setAirlines(resp.data.data);
+      })
+      .catch((resp) => console.log(resp));
+  }, [airlines.length]);
+
+  const list = airlines.map((item) => {
+    return <li key={item.attributes.name}>{item.attributes.name}</li>;
+  });
+
+  return (
+    <Fragment>
+      <div> This is the Airlines#index view for the app</div>
+      <ul>{list}</ul>
+    </Fragment>
+  );
 };
 
 export default Airlines;
