5b9fb5a786852a5ea727f0e78a4771a7bb1c6a4d
MICHAEL <MJENNINGS6@gmail.com>
2021-06-13 22:49:10 -0400

    reset bc commit msg was also wrong

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -11,7 +11,6 @@ class Airline < ApplicationRecord
 
   # method to caputre avg score
   def avg_score
-    return 0 unless reviews.count.positive?
     reviews.average(:score).round(2).to_f
   end
 
