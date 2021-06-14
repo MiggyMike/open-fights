e0f20156c8f1a7dfb11110a49b7911216ddd38fc
MICHAEL <MJENNINGS6@gmail.com>
2021-06-13 22:34:06 -0400

    added avg_score to attributes serializer

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -11,6 +11,7 @@ class Airline < ApplicationRecord
 
   # method to caputre avg score
   def avg_score
+    return 0 unless reviews.count.positive?
     reviews.average(:score).round(2).to_f
   end
 
