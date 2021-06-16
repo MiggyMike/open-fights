578aa52f1bfaf11289d041d699de8a285bd6ee42
MICHAEL <MJENNINGS6@gmail.com>
2021-06-16 13:58:12 -0400

    added layout to page with  header comp and load helper to access nested fields and pass those props

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from './Header';
 
 const Airline = (props) => {
   const [airline, setAirline] = useState({});
   const [review, setReview] = useState({});
+  // set this as false - this lets us know when the data has been loaded - add to useEffect - set a boolean to update - helper for digging into nested objects
+  const [loaded, setLoaded] = useState(false);
 
   //
   useEffect(() => {
-    // console.log(props);
+    console.log(props);
 
     // airlines/:slug
     const slug = props.match.params.slug;
@@ -16,10 +19,30 @@ const Airline = (props) => {
 
     axios
       .get(url)
-      .then((resp) => console.log(resp))
+      .then((resp) => {
+        // console.log(resp);
+        setAirline(resp.data);
+        setLoaded(true);
+      })
       .catch((resp) => console.log(resp));
   }, []);
-  return <div> This is the Airline#show view for the app</div>;
+  return (
+    <div className='wrapper'>
+      <div className='col'>
+        {/* added the loaded wrapper to header */}
+        {loaded && (
+          <Header
+            attributes={airline.data.attributes}
+            reviews={airline.included}
+          />
+        )}
+        <div className='reviews'></div>
+      </div>
+      <div className='col'>
+        <div className='review-form'>Form Goes Here</div>
+      </div>
+    </div>
+  );
 };
 
 export default Airline;
