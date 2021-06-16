9b371232b51384eea34708b05a2401fa7e7c8bd7
MICHAEL <MJENNINGS6@gmail.com>
2021-06-16 11:49:31 -0400

    set airline show to use hooks to capture and use state/data

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Airline = () => {
+const Airline = (props) => {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+
+  //
+  useEffect(() => {
+    // console.log(props);
+
+    // airlines/:slug
+    const slug = props.match.params.slug;
+    // api/v1/airlines/:slug
+    const url = `/api/v1/airlines/${slug}`;
+
+    axios
+      .get(url)
+      .then((resp) => console.log(resp))
+      .catch((resp) => console.log(resp));
+  }, []);
   return <div> This is the Airline#show view for the app</div>;
 };
 
