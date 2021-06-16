b63b1a976c4bfe975e68a2cc941894a7c89cab63
MICHAEL <MJENNINGS6@gmail.com>
2021-06-16 14:23:20 -0400

    styled with styled components

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,21 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 50px 100px 50px 0;
+  font-size: 30px;
+
+  img {
+    margin-right: 10px;
+    height: 60px;
+    width: 60px;
+    border-radius: 100%;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    margin-bottom: -8px;
+  }
+`;
+const TotalReviews = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+`;
+// const StarRating = styled.div``;
+const TotalOutOf = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px 0;
+`;
 
 const Header = (props) => {
+  // object desructuring
   const { name, image_url, avg_score } = props.attributes;
+  // grabbing all reviews count
   const total = props.reviews.length;
 
   return (
-    <div className='wrapper'>
+    <Wrapper>
       <h1>
         <img src={image_url} alt={name} />
         {name}
       </h1>
       <div>
-        <div className='totalReviews'>{total} User Reivews</div>
+        <TotalReviews>{total} User Reivews</TotalReviews>
         <div className='starRating'></div>
-        <div className='totalOutOf'>{avg_score}</div>
+        <TotalOutOf>{avg_score} out of 5 </TotalOutOf>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
