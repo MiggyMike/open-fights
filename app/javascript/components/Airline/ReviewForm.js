c8a7536d616c1cb56be0aeeac1fd5a38c6a548b2
MICHAEL <MJENNINGS6@gmail.com>
2021-06-16 20:38:18 -0400

    updated with event handlers props

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -3,13 +3,22 @@ import React from 'react';
 const ReviewForm = (props) => {
   return (
     <div className='Wrapper'>
-      <form>
-        <div>Have an experience with [AirlineName]? Share your review!</div>
-        <div className='field'>
-          <input type='text' name='title' placeholder='Review Title'></input>
+      <form onSubmit={props.handleSubmit}>
+        <div>
+          Have an experience with {props.attributes.name}? Share your review!
         </div>
         <div className='field'>
           <input
+            onChange={props.handleChange}
+            value={props.review.title}
+            type='text'
+            name='title'
+            placeholder='Review Title'></input>
+        </div>
+        <div className='field'>
+          <input
+            onChange={props.handleChange}
+            value={props.review.description}
             type='text'
             name='description'
             placeholder='Review Description'></input>
