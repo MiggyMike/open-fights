384fb655eda9210280aa7691ec5def6379b084ad
MICHAEL <MJENNINGS6@gmail.com>
2021-06-16 21:04:57 -0400

    created/set ratingOptions

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const ReviewForm = (props) => {
+  const ratingOptions = [5, 4, 3, 2, 1].map((score, index) => {
+    return (
+      <Fragment>
+        <input
+          type='radio'
+          value={score}
+          name='rating'
+          onChange={() => console.log('selected:', score)}
+          id={`rating-${score}`}
+        />
+        <label></label>
+      </Fragment>
+    );
+  });
+
   return (
     <div className='Wrapper'>
       <form onSubmit={props.handleSubmit}>
@@ -26,7 +41,7 @@ const ReviewForm = (props) => {
         <div className='field'>
           <div className='rating conatiner'>
             <div className=' rating-title-text'>Rate this Airline</div>
-            [Star Rating Goes Here]
+            {ratingOptions}
           </div>
         </div>
         <button type='submit'>Submit Your Review</button>
